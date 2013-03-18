6e6f1da63d54310ff535566054524f909389fda4
Andreas Wilm - CMB5 <wilma@gis.a-star.edu.sg>
2013-03-18 15:37:17 +0800

    Added more comments

--- decont.sh
+++ decont.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Decontaminate FastQ
-#
-# Largely based on bwa_unique.sh
+# Decontaminate FastQ (single-end, paired-end, optionally gzipped) by
+# mapping against a contamination source
 
 echoinfo() {
     echo "INFO: $@" 1>&2
@@ -12,20 +11,24 @@ echofatal() {
 }
 
 # defaults
+# illumina quality encoding
 illumina=0
+# number of threads to use
 threads=4
+# keep temp directory
 keep_temp=0
+# default picard directory; override with env var PICARDDIR
 PICARDDIR_DEFAULT="/mnt/software/stow/picard-1.74/bin/"
 
 usage() {
     # keep in sync with arg parsing below
 cat <<EOF
-$(basename $0): wrapper decontaminating a fastq file
+$(basename $0): decontaminate fastq
 
 Performs a mapping of given SR/PE reads (gzip supported) with BWA
-against given reference / source of contamination and produces a BAM
-file with contaminated reads and a new fastq file with clean reads
-(qualities will be Sanger encoded).
+against given source of contamination and produces a BAM file with
+contaminated reads and new fastq file(s) with clean reads (qualities
+will be Sanger encoded).
 
 Prerequisites: BWA, Picard and samtools. Point the PICARDDIR
 environment variable to your picard installation (will use
@@ -49,9 +52,9 @@ EOF
 
 # check for required programs
 #
-for bin in java samtools; do
+for bin in java samtools bwa; do
     if ! which $bin >/dev/null 2>&1; then
-        echofatal "couldn't find $bin. make sure it's in your path"
+        echofatal "Couldn't find $bin. make sure it's in your path"
         exit 1
     fi
 done
@@ -59,7 +62,7 @@ test -z "$PICARDDIR" && export PICARDDIR=$PICARDDIR_DEFAULT
 # check for picard needed for samtofastq adding (needed for GATK)
 picard_samtofastq_jar=${PICARDDIR}/SamToFastq.jar
 if [ ! -s $picard_samtofastq_jar ]; then
-    echofatal "couldn't find Picard's $(basename picard_samtofastq_jar). Please set PICARDDIR to your Picard installation"
+    echofatal "Couldn't find Picard's $(basename picard_samtofastq_jar). Please set PICARDDIR to your Picard installation"
     exit 1
 fi
 
@@ -85,7 +88,7 @@ while [ "$1" != "" ]; do
         --illumina )
             illumina=1
             ;;
-        --keep )
+        -k | --keep )
             keep_temp=1
             ;;
         -o | --outprefix )           
@@ -136,21 +139,19 @@ if [ -z "$tmpdir" ]; then
 fi
 echoinfo "Using temp dir $tmpdir"
 
-# set up output file names
+# set first batch of output file names that we need early on
 contbam=${outprefix}_cont.bam
 fastq_clean_1=${outprefix}_1.fastq
 if [ ! -z $fastq2 ]; then
     fastq_clean_2=${outprefix}_2.fastq
 fi
 
-if [ -s $fastq_clean_1 ] || [ -s ${fastq_clean_1}.gz ] || [ -s $contbam ] ; then
-    echofatal "refusing to overwrite already existing file"
-    exit 1
-fi
-
-
-# index reference if necessary
-test -s ${reffa}.pac || bwa index $reffa || exit 1
+for f in $fastq_clean_1 ${fastq_clean_1}.gz $contbam; do
+    if [ -s $f ]; then
+        echofatal "refusing to overwrite already existing file $f"
+        exit 1
+    fi
+done
 
 
 #
@@ -158,8 +159,12 @@ test -s ${reffa}.pac || bwa index $reffa || exit 1
 # please get on with it?
 #
 
+# index reference if necessary
+test -s ${reffa}.pac || bwa index $reffa || exit 1
+
 allbam=$tmpdir/all.bam
 bwalog=$tmpdir/bwa.log
+
 
 # STEP 1: bwa aln
 # --------------------------------------------------------------------
@@ -167,10 +172,9 @@ bwalog=$tmpdir/bwa.log
 # -q 3: remove q3 in accordance with illumina guidelines
 bwa_aln_extra_args="-t $threads -q 3"
 if [ $illumina -eq 1 ]; then
-    bwa_aln_extra_args="$bwa_aln_extra_args -I"
     # -I: if phred qualities are ascii64, ie. Illumina 1.3-1.7
+    bwa_aln_extra_args="$bwa_aln_extra_args -I"
 fi
-
 
 # bwa aln for each fastq. skip if sai already exists
 sais=""
@@ -225,31 +229,30 @@ echoinfo "BWA sam[sp]e done"
 
 
 
-
 # STEP 3: split into contaminated reads (BAM) and clean FastQ
 # --------------------------------------------------------------------
 
 # Not using samtools view with -f/-F (0x4 segment unmapped, 0x8 next
 # segment in the template unmapped), but awk to save one reading
-# operation
-#
-# In case of PE reads, we consider clean if neither read nor mate are
-# mapped read 0x4 and mate 0x8 unmapped. Note, samtools view -f 12
-# (note: not 0x12!) does not allow for mapped reads and unmapped mates
-# and vice versa. At the same time you can give -F/-f only once to
-# samtools.
+# operation and also the following reason: In case of PE reads, we
+# consider clean if neither read nor mate are mapped read 0x4 and mate
+# 0x8 unmapped. Note, samtools view -f 12 (note: not 0x12!) does not
+# allow for mapped reads and unmapped mates and vice versa. At the
+# same time you can give -F/-f only once to samtools. Another
+# advantage of using awk is that you can avoid a nasty BWA bug, which
+# results in inconsistent mate-pair info. See
+# http://www.biostars.org/p/60100/ and
+# http://www.biostars.org/p/60001/
 #
 # Use picard for bam2fastq because it's extra pedantic. Good
 # alternative would be
 # http://www.hudsonalpha.org/gsl/software/bam2fastq.php which has the
-# (dis)advantage of simply skipping unpaired mate-pairs.
-#
-# With LENIENT Picard will ignore errors like "Error parsing text SAM
+# (dis)advantage of simply skipping unpaired mate-pairs. With
+# "LENIENT" Picard will ignore errors like "Error parsing text SAM
 # file. MAPQ should be 0 for unmapped read." for cicular chromosomes.
 #
-# All communication via fifo to save io and only save BAM/gz.
+# All communication via fifo to save space and only save BAM/gz.
 # Disadvantage: can't catch errors directly, only via log.
-#
 
 # setup fifos
 fastq_clean_1_fifo=$tmpdir/$(basename $fastq_clean_1 fastq)fifo
