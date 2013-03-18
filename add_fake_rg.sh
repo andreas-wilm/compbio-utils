928ee353e22f0f69001a09223b312d16f4ce9183
Andreas Wilm - CMB5 <wilma@gis.a-star.edu.sg>
2013-03-18 15:36:20 +0800

    Initial commit

---
+++ add_fake_rg.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Add minimal fake default read group to a BAM file. GATK requires
+# read groups (you wonder why there's no option to assume a default
+# one, but never mind).
+
+bam="$1";
+test -n $bam || exit 1
+test -s $bam || exit 1
+
+# lazy implementation: parse twice. First head and add RG declaration.
+# Then assign RG to reads.
+samtools view -H $bam && exit 1
+echo -e "@RG\tID:1\tPL:illumina\tPU:platform-unit-dummy\tLB:library-dummy\tSM:sample-name-dummy\tCN:sequencing-center-dummy";
+samtools view $bam | \
+     awk 'BEGIN {FS="\t"; OFS="\t"} {printf "%s\tRG:Z:1\n", $0}';
