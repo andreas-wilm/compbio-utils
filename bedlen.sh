34b85ea4dfa28eccfd45f1fd4306f75cfd438fed
Andreas Wilm <wilma@gis.a-star.edu.sg>
2013-11-19 23:26:57 +0800

    Now using all input files

--- bedlen.sh
+++ bedlen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # give bed file as last arg or pipe in
-awk '{if (/^[^#]/) {d[$1]+=($3-$2)}} END {for (c in d) {printf "%s\t%d\n", c, d[c]; s+=d[c]}; printf "TOTAL\t%d\n", s}' $1
+awk '{if (/^[^#]/) {d[$1]+=($3-$2)}} END {for (c in d) {printf "%s\t%d\n", c, d[c]; s+=d[c]}; printf "TOTAL\t%d\n", s}' $@
