6ff748ba043c1fda287cd22f509b8b12139a9e25
Andreas Wilm - CMB5 <wilma@gis.a-star.edu.sg>
2014-03-17 11:02:26 +0800

    fixed wrong exit status check

--- add_fake_rg.sh
+++ add_fake_rg.sh
@@ -5,12 +5,12 @@
 # one, but never mind).
 
 bam="$1";
-test -n $bam || exit 1
-test -s $bam || exit 1
+test -n "$bam" || exit 1
+test -s "$bam" || exit 1
 
 # lazy implementation: parse twice. First head and add RG declaration.
 # Then assign RG to reads.
-samtools view -H $bam && exit 1
+samtools view -H $bam || exit 1
 echo -e "@RG\tID:1\tPL:illumina\tPU:platform-unit-dummy\tLB:library-dummy\tSM:sample-name-dummy\tCN:sequencing-center-dummy";
 samtools view $bam | \
      awk 'BEGIN {FS="\t"; OFS="\t"} {printf "%s\tRG:Z:1\n", $0}';
