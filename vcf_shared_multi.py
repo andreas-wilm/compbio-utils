82d06679a67903abfdf501441ac5261b9ff9eacb
Andreas Wilm - CMB5 <wilma@gis.a-star.edu.sg>
2015-02-06 17:25:39 +0800

    vcf_shared_multi.py: added command line parameters defining how much counts as shared

--- vcf_shared_multi.py
+++ vcf_shared_multi.py
@@ -2,10 +2,11 @@
 """Print variants in at least two of given vcf files. Output is not sorted
 """
 
+
 import sys
+import os
 import vcf
 from collections import Counter
-
 
 
 def key_to_var(key):
@@ -16,8 +17,6 @@ def key_to_var(key):
 
 def var_key(var):
     """generate a key for a (pyvcf) variant"""
-
-
     if len(var.ALT)>1:# FIXME
         sys.stderr.write("Multi allelic entry found. Keeping ony first of %s\n" % var)
     # .ALT is not as string
@@ -27,11 +26,17 @@ def var_key(var):
 
 
 if __name__ == "__main__":
-    VCFS = sys.argv[1:]
-    assert len(VCFS)>1, ("Expected at least two vcf files as argument")
+    try:
+        atleast = int(sys.argv[1])
+        vcfs = sys.argv[2:]
+        assert len(vcfs)>1
+    except:
+         sys.stderr.write("ERROR: missing or wrong arguments\n")
+         sys.stderr.write("Usage: %s min-no-to-consider-shared vcf1 vcf2 [... vcfn]\n" % os.path.basename(sys.argv[0]))
+         sys.exit(1)
     
     all_var_keys = []
-    for f in VCFS:
+    for f in vcfs:
         vcfr = vcf.Reader(filename=f)
         for v in vcfr:
             if not v.FILTER:
@@ -40,6 +45,6 @@ if __name__ == "__main__":
     # plain vcf output. creating new record for vcf.Writer would be more difficult
     print "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO"
     ctr = Counter(all_var_keys)
-    for k in [k for (k, v) in ctr.iteritems() if v > 1]:
+    for k in [k for (k, v) in ctr.iteritems() if v >= atleast]:
         print key_to_var(k)
     
