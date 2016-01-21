746b140db4e65e6ed0e7ee58b34628ea060718db
Andreas WILM (GIS) <wilma@gis.a-star.edu.sg>
2016-01-21 15:57:33 +0800

    samplename now user var

--- add_fake_rg.sh
+++ add_fake_rg.sh
@@ -8,9 +8,12 @@ bam="$1";
 test -n "$bam" || exit 1
 test -s "$bam" || exit 1
 
+samplename="$2"
+test -n "$samplename" || samplename="sample-name-dummy"
+
 # lazy implementation: parse twice. First head and add RG declaration.
 # Then assign RG to reads.
 samtools view -H $bam || exit 1
-echo -e "@RG\tID:1\tPL:illumina\tPU:platform-unit-dummy\tLB:library-dummy\tSM:sample-name-dummy\tCN:sequencing-center-dummy";
+echo -e "@RG\tID:1\tPL:illumina\tPU:platform-unit-dummy\tLB:library-dummy\tSM:${samplename}\tCN:sequencing-center-dummy";
 samtools view $bam | \
      awk 'BEGIN {FS="\t"; OFS="\t"} {printf "%s\tRG:Z:1\n", $0}';
